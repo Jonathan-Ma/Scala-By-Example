08d758bdb4727e2f37341a2ec2b56c3c15e7c2a5
jon <jma66@hawaii.edu>
2021-08-17 23:28:12 -1000

    Now on section 8.5

--- src/main/scala/Chapter8/Ch8.scala
+++ src/main/scala/Chapter8/Ch8.scala
@@ -13,7 +13,16 @@ object Ch8 {
      // 8.1
     val set = new EmptySet[Num].incl(Num(2.0)).incl(Num(3.0))
     println(set.contains(Num(2.0)))
+    //8.5 tuples
+    val xy = divmod2(3,8)
+    println("Quotient: " + xy._1 + ", remainder: " + xy._2)
+    divmod2(3, 8) match {
+      case Tuple2(n, d) =>
+        println("Quotient: " + n + ", remainder: " + d)
+    }
   }
+
+
   // Scala classes can have type parameters
   abstract class Stack[A] {
     def push(x: A): Stack[A] = new NonEmptyStack[A](x, this)
@@ -130,6 +139,91 @@ object Ch8 {
    * 8.3 Lower Bounds
    ******************************************************************/
   /*
+   * There is upper bound which is annotated as: A <: T, this means
+   * A has to be a subtype of T.
+   * Suppose we have the following:
+   */
+    trait Ingredient{
+      def isDelicious: Boolean
+    }
+    case object Chili extends Ingredient {
+      def isDelicious = true
+    }
+    case object Tomato extends Ingredient {
+      def isDelicious = true
+    }
+    // A filterDelicious function that filters a list of ingredients and returns delicious ones
+    def filterDelicious(ingredients: List[Ingredient]): List[Ingredient] = ingredients.filter(_.isDelicious)
+    val result: List[Ingredient] = filterDelicious(List(Chili, Chili))
+    // Or we can do List[A <: Ingredient] and yield same result
+    def filterDelicious1[A <: Ingredient](ingredients: List[A]): List[A] = ingredients.filter(_.isDelicious)
+    // this is because A is a subtype of Ingredient so we can still use .isDelicious
+
+    /*
+     * For a lower bound variance List[A >: Ingredient], suppose we have
+     * a Food trait and Ingredient extends Food like so:
+     * trait Food
+     * trait Ingredient extends Food{
+     *  def isDelicious: Boolean
+     * }
+     * case object Chili extends Ingredient {
+     *  def isDelicious = true
+     * }
+     * case object Tomato extends Ingredient {
+     *  def isDelicious = true
+     * }
+     * Then with filterDelicious we can do:
+     * def filterDelicious1[A >: Ingredient](ingredients: List[A]): List[A]
+     * and A can be Food
+   * Lower bound is the same concept indicated as A >: T which means
+   * A is lower bounded by T, so A can be T or a supertype of T.
    *
    */
+
+  /*******************************************************************
+   * 8.5 Tuples
+   ******************************************************************/
+  /*
+   * We can use tuples when we need to return multiple results, for example
+   * a divmod function that returns the integer quotient and remainder of the
+   * two integers.
+   * We could use a class to hold the two results as so:
+   */
+  case class TwoInts(first: Int, second: Int)
+  def divmod(x: Int, y: Int):TwoInts = new TwoInts(x/y, x%y)
+  /*
+   * But the problem is that having to create a class everytime to hold
+   * values is tedious, thus we use tuples
+   */
+  def divmod2(x: Int, y:Int) = new Tuple2[Int, Int](x / y, x % y )
+  // How to access the elements
+  // val xy = divmod2(3,8)
+  // println("Quotient: " + xy._1 + ", remainder: " + xy._2)
+  /*
+   * Tuple2 is defined in scala as
+   * case class Tuple2[A, B](._1: A, ._2: B)
+   * therefore we have to use new prefix to create a Tuple, there are also
+   * Tuple1, Tuple3...etc
+   */
+   //Because its a case class, we can access Tuple elements by matching
+  //   divmod2(x, y) match {
+  //    case Tuple2(n, d) =>
+  //      println("Quotient: " + n + ", remainder: " + d)
+  //  }
+  /*
+   * "Tuple_" can be omitted, hence, we can write divmod like this:
+   */
+   def divmod3(x: Int, y: Int): (Int, Int) = (x / y, x % y)
+  // Basically tuple becomes a type kind of
+  // the matching for this is:
+  /*
+  divmod3 match {
+    case (n, d) => println("Quotient: " + n + ", remainder: " + d)
+  }
+   */
+
+  /*******************************************************************
+   * 8.6 Functions
+   ******************************************************************/
+  
 }
