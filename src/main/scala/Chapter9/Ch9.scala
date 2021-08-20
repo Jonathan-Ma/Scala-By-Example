b9ccca57e3d54222e8d6a85ef447649241eae16e
jon <jma66@hawaii.edu>
2021-08-19 23:41:31 -1000

    Finished exercise 9.1.1

--- src/main/scala/Chapter9/Ch9.scala
+++ src/main/scala/Chapter9/Ch9.scala
@@ -1,14 +1,18 @@
 package Chapter9
 
 object Ch9 {
+  def main(args: Array[String]): Unit = {
+    for(x <- fruit)
+      println(x)
+  }
   /*******************************************************************
    * Chapter 9 Lists
    ******************************************************************/
   /*
    * A list is represented as:
    */
-  val intList = List(1, 2, 3)
-  val stringList = List("a", "b", "fruit")
+  val intList: List[Int] = List(1, 2, 3)
+  val stringList: List[String] = List("a", "b", "fruit")
   val empty = List()
   /*
    * Lists are similar to arrays but with three key differences:
@@ -20,5 +24,17 @@ object Ch9 {
   /*******************************************************************
    * 9.1 Using Lists
    ******************************************************************/
-  
+  /*
+   * List constructors. All lists are built from two more fundamental
+   * constructors, Nil and '::'(Cons operator).
+   * We can use the Cons infix operator to write the lists above:
+   */
+  val fruit: List[String] = "Apple" :: "Pineapple" :: "Guava" :: "Tomato" :: Nil
+  // Notice that if Nil is omitted this won't work
+  /*
+   * Basic operations on Lists.
+   * 1. isEmpty: returns true if list is empty
+   * 2. Tail: returns ALL elements after the first element
+   * 3. Head: returns first element
+   */
 }
